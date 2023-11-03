c1d34cf9ca2eca23bde4eb5f250fb0a5e7f61872
Kelpin <141326351+Internite@users.noreply.github.com>
2023-11-03 23:47:29 +0700

    Hii !

---
+++ bain.sql
@@ -0,0 +1,14 @@
+SELECT
+    ci.name
+FROM city ci
+INNER JOIN country co
+    ON ci.countrycode = co.code
+WHERE co.continent = "Africa"
+
+
+SELECT
+    SUM(ci.population)
+FROM city ci
+INNER JOIN country co
+    ON ci.countrycode = co.code
+WHERE co.continent = "Asia"
